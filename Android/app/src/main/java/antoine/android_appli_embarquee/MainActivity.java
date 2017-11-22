ba6074090dfd90c61c6bbcf5afc267de19a75a49
TheDoctor84 <ant984@free.fr>
2017-11-22 17:01:53 +0100

    Test creation projet Android

---
+++ Android/app/src/main/java/antoine/android_appli_embarquee/MainActivity.java
@@ -0,0 +1,13 @@
+package antoine.android_appli_embarquee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
