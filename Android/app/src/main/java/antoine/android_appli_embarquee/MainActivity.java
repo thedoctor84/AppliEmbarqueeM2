08ec63a054c13c2522ab8006c099fdec5fe57ee3
TheDoctor84 <ant984@free.fr>
2017-12-20 20:10:57 +0100

    integration camera

--- Android/app/src/main/java/antoine/android_appli_embarquee/MainActivity.java
+++ Android/app/src/main/java/antoine/android_appli_embarquee/MainActivity.java
@@ -3,22 +3,39 @@ package antoine.android_appli_embarquee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.niqdev.mjpeg.DisplayMode;
+import com.github.niqdev.mjpeg.Mjpeg;
+import com.github.niqdev.mjpeg.MjpegView;
+
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MjpegView mjpegView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        JoystickView joystick = (JoystickView) findViewById(R.id.joystickView);
+        int TIMEOUT = 10; //seconds
+
+        mjpegView = findViewById(R.id.camera);
+
+        Mjpeg.newInstance()
+                .open("http://192.168.137.199/html/cam_pic_new.php", TIMEOUT)
+                .subscribe(inputStream -> {
+                    mjpegView.setSource(inputStream);
+                    mjpegView.setDisplayMode(DisplayMode.BEST_FIT);
+                    mjpegView.showFps(true);
+                });
+        /*JoystickView joystick = (JoystickView) findViewById(R.id.joystickView);
         joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
             public void onMove(int angle, int strength) {
                 System.out.println("ANGLE = "+angle);
                 System.out.println("strength = "+strength);
             }
-        });
+        });*/
 
     }
 
